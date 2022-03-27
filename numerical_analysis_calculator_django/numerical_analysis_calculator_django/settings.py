9d1ec9766e536445e981324b941c598e4e500a2b
Numbers_00 <kylemariangelo@gmail.com>
2022-03-27 11:21:55 +0800

    misc

--- numerical_analysis_calculator_django/numerical_analysis_calculator_django/settings.py
+++ numerical_analysis_calculator_django/numerical_analysis_calculator_django/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ua9al7*bd*_rpv2%&qj-tje)gec($#bbd!!!8#wthm-j$wf=hl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
